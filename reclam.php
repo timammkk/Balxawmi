6dd347c28a73081c26b92564b3c91ef2208f4b6a
fatima <fatimamamadoukane@esp.sn>
2022-06-30 21:34:05 +0000

    premier commit

---
+++ reclam.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contactez-moi</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+   
+    <link href="https://fonts.googleapis.com/css2?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="img">
+    <nav class="navbar navbar-expand-lg ">
+    <!--<div class="logo"><i class="bi bi-caret-left"></i>BALXAWMI</div>-->
+
+      <div class="container-fluid">
+       
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#home">Acceuil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">A propos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#products">Produits</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="C:\Users\dell\OneDrive\Bureau\projetWeb\projetWeb\services.html">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#contacts">Contacs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="reclamations.html" target="_blank"> Reclamation</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#login"> <span><i class="bi bi-person-fill"></i> Login</span></a>
+            </li>
+          </ul>
+         
+        </div>
+      </div>
+    </nav>
+
+  </div>
+  
+<section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1">
+                    <form id="contact-form" method="post" action="" role="form">
+                                <label for="email">Email*</label>
+                                <input type="text" id="email" name="email"  class="form-control" placeholder="Votre email">
+                                <p class="comments"></p>
+                                <label for="message">Message*</label>
+                                <textarea name="message" id="message" class="form-control" placeholder="Votre message" rows="4" ></textarea>
+                                <p class="comments"></p>
+                                <input type="submit" class="button1" value="Envoyer">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+      <div class="footer-content" id="about">
+        <div class="bloc">
+          <h4>NOS SERVICES</h4>
+        <p>-Developpement <br> -Web Design <br> -Devis et Vente </p>
+        </div>
+        <div class="bloc">
+          <h4>NOS CONTACTS</h4>
+        <p> <span>Phone:</span> +1 5589 55488 55 <br> <span>Email: </span> alexandre@esp.sn </p>
+        </div>
+        
+        </div>
+      </div>
+      <div class="bloc">
+        <ul class="socials">
+          <li><a href="http://www.instagram.com"><i class="bi bi-instagram"></i></a></li>
+          <li><a href="http://www.facebook.com"><i class="bi bi-facebook"></i></a></li>
+          <li><a href="http://www.linkedin.com"><i class="bi bi-linkedin"></i></a></li>
+          <li><a href="http://www.whatsapp.com"><i class="bi bi-whatsapp"></i></a></li>
+        </ul>
+        </div>
+        <div class="copyright">
+          <h5>Copyright © Balxawmi Groupes Services</h5>
+        </div>
+    </footer>
+   
+</body>
+</html>
